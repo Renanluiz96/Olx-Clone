4da994b1d6c7afeccce520669ab2f4bb85335548
Renan Luiz <renanluizz1996@gmail.com>
2023-04-12 07:22:12 -0300

    Criando a estrutura do header , e os menus.

--- Front-end/src/components/MainTemplate.tsx
+++ Front-end/src/components/MainTemplate.tsx
@@ -1,4 +1,7 @@
+import '../App.css'
+
 import { MainRoutes } from "../routes/routes";
+
 import { Template } from "./MainComponents";
 import Header from "./PartialComponents/Header";
 import Footer from "./PartialComponents/Footer";
--- Front-end/src/components/PartialComponents/Header/styled.ts
+++ Front-end/src/components/PartialComponents/Header/styled.ts
@@ -1,6 +1,69 @@
 import styled from "styled-components";
 
 export const HeaderArea = styled.div`
-    height: 100px;
-    background-color: #ccc;
+    background-color: #FFF;
+    height: 60px;
+    border-bottom: 1px solid #ccc;
+
+    .container {
+        max-width: 554px;
+        margin: auto;
+        display: flex;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    .logo {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        height: 60px;
+        
+        .logo-1, .logo-2, .logo-3 {
+            font-size: 22px;
+            font-weight: bold;
+        }
+        .logo-1 { color: #FF0000 }
+        .logo-2 { color: #00ff00 }
+        .logo-3 { color: #0000ff }
+    }
+
+    nav {
+        padding: 10px 0;
+
+        ul, li {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+        ul {
+            display: flex;
+            align-items: center;
+            height: 40px;
+        }
+        li {
+            margin: 0 10px;
+
+            a {
+                color: black;
+                font-size: 14px;
+
+                &:hover {
+                    color: #777;
+                }
+                //Quando achar dentro dos links um link com a classe button , aplicar somente no link com a classe
+                &.button {
+                    background-color: #FF7100;
+                    border-radius: 4px;
+                    color: #FFF;
+                    padding: 5px 10px;
+                }
+                &.button:hover {
+                    background-color: #e57700 ;
+                }
+            }
+        }
+    }
 `;
