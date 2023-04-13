4225a9fbb1ddf3cdd790b94ec1e138aa0eb4ac54
Renan Luiz <renanluizz1996@gmail.com>
2023-04-13 05:50:13 -0300

    Instalando lib js-cookie e o type dela no projeto

--- Front-end/src/redux/reducers/Reducers.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import userReducer from './userReducer'
-
-
-//Combinar todos os reducers que sera criado em apenas 1.
-export default combineReducers({
-    user:userReducer
-})
--- Front-end/src/redux/reducers/userReducer.ts
+++ Front-end/src/redux/reducers/userReducer.ts
@@ -1,27 +1,3 @@
-/*
-    export type StateType = {
-        email: string | '' ;
-    }
-
-    export type ActionType = {
-        type: string;
-        payload: StateType
-    }
-
-    const initialState: StateType = {
-        email:''
-    }
-
-    export default (state = initialState, action: ActionType) => {
-
-        if(action.type === 'SET_EMAIL') {
-            return { ...state, email:action.payload.email };
-        }
-
-        return state
-    }
-*/
-
 import { createSlice } from "@reduxjs/toolkit";
 
 export const slice = createSlice({
